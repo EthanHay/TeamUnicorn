b7d0a4c4deb98a3fbaba1105a5123d3a6c754918
Heidi323 <heidi.brede@my.jcu.edu.au>
2015-05-13 14:13:12 +1000

    Signup form
    Signup form with database

---
+++ milestone2/signup/signup.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<link href="signupstyle.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+<h1>Sign-up form</h1>
+
+<form id="register" name="register" method="post" action="signupprocess.php">
+	<p>
+	<label class="label" for="firstname">First Name</label>
+    <input type="text" name="firstname" id="firstname" required>
+    </p>
+    <p>
+    <label class="label" for="surname">Surname</label>
+    <input type="text" name="surname" id="surname" required>
+    </p>
+    <p>
+    <label class="label" for="street">Address</label>
+    <input type="text" name="street" id="street" required>
+    </p>
+    <p>
+    <label class="label" for="postcode">Post code</label>
+    <input type="text" name="postcode" id="postcode" pattern=".{4,4}" required title="4 characters needed" size="4"><br>
+    </p>
+    <p>
+    <label class="label" for="suburb">Suburb</label>
+    <input type="text" name="suburb" id="suburb" required>
+    </p>
+    <p>
+    <label class="label" for="state">State</label>
+    <select name="state" id="state"><option>ACT</option><option>NSW</option><option>NT</option><option selected>Qld</option><option>SA</option><option>Tas
+    </option><option>Vic</option><option>WA</option></select>
+	</p>
+    <p>
+    <label class="label" for="phoneday">Phone Day</label>
+    <input type="text" pattern=".{10,10}" required title="please check the phone number. No country code needed"
+    name="phoneday" id="phoneday" >
+    </p>
+    <p>
+    <label class="label" for="phoneeve">Phone Evening</label>
+    <input type="text" name="phoneeve" id="phoneeve" >
+    </p>
+    <p>
+    <label class="label" for="email">Email</label>
+    <input type="email" name="email" id="email" required>
+    </p>
+    <p>
+    <label class="label" for="username">Username</label>
+    <input type="text" name="username" id="username" required>
+    </p>
+    <p>
+    <label class="label" for="password">Password</label>
+    <input type="password" name="password" id="password" pattern=".{6,}" required title="Please make sure your password has more than 6 characters">
+    </p>
+    <p>
+    <input type="submit" name="submit" id="submit" value="Become a member">
+    </p>
+    </form>
+    
+    
+</body>
+</html>
---
+++ milestone2/signup/signupprocess.php
@@ -0,0 +1,77 @@
+<?php
+/* This code runs the SQL queries and outputs what happens as a result of the queries.
+   It would be possible to have this code set messages in a session variable and pass this on to another page 
+   (redirect with the header method) instead of printing the results here. 
+   The X option demonstrates this ("silent" delete).
+*/
+include("signupconnect.php");
+
+$debugOn = true;
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Artist Information Processing</title>
+</head>
+
+<body>
+<h1>Results</h1>
+<?php
+echo "<h2>Form Data</h2>";
+echo "<pre>";
+print_r($_REQUEST); // a useful debugging function to see everything in an array, best inside a <pre> element
+echo "</pre>";
+// execute the appropriate query based on which submit button (insert, delete or update) was clicked
+
+if ($_REQUEST['submit'] == "Become a member")
+{
+	$sql = "INSERT INTO members (firstname, surname, address, postcode, suburb, state, phoneday, phoneeve, email, username, password, status) VALUES 
+	('$_REQUEST[firstname]', '$_REQUEST[surname]', '$_REQUEST[street]', '$_REQUEST[postcode]', '$_REQUEST[suburb]', 
+	'$_REQUEST[state]','$_REQUEST[phoneday]','$_REQUEST[phoneeve]','$_REQUEST[email]','$_REQUEST[username]','$_REQUEST[password]', 'free')";
+	echo "<p>Query: " . $sql . "</p>\n<p><strong>"; 
+	if ($dbh->exec($sql))
+		echo "Inserted $_REQUEST[name]";
+	else
+		echo "Not inserted"; // in case it didn't work - e.g. if database is not writeable
+}
+
+else {
+	echo "This page did not come from a valid form submission.<br />\n";
+}
+echo "</strong></p>\n";
+
+// Basic select and display all contents from table 
+
+echo "<h2>Members in Database Now</h2>\n";
+$sql = "SELECT * FROM members";
+$result = $dbh->query($sql);
+$resultCopy = $result;
+
+if ($debugOn) {
+	echo "<pre>";	
+// one row at a time:
+/*	$row = $result->fetch(PDO::FETCH_ASSOC);
+	print_r($row);
+	echo "<br />\n";
+	$row = $result->fetch(PDO::FETCH_ASSOC);
+	print_r($row);
+*/
+// all rows in one associative array
+	$rows = $result->fetchall(PDO::FETCH_ASSOC);
+	echo count($rows) . " records in table<br />\n";
+	print_r($rows);
+	echo "</pre>";
+	echo "<br />\n";
+}
+//foreach ($dbh->query($sql) as $row)
+//{
+//	print $row[name] .' - '. $row[phone] . "<br />\n";
+//}
+
+// close the database connection 
+$dbh = null;
+?>
+<p><a href="signup.php">Return to database test page</a></p>
+</body>
+</html>
