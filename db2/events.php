4bddc0968d671a1ffa463d152962f1c0982f3c3c
EThan296 <EThan296@users.noreply.github.com>
2015-05-19 17:22:14 +1000

    Update events.php

--- db2/events.php
+++ db2/events.php
@@ -29,20 +29,20 @@ foreach ($dbh->query($sql) as $row)
 $dt = strtotime($row[date]);	
 $date = date("d/m/Y", $dt);
 $day = date("D", $dt);	
-if($date == $today or $date > $today) /*Does not show outdated events */
-{
+if($date == $today or $date > $today) {
 	
 echo "
 <div class=\"eventbox\">
 <img class=\"eventimg\" src=/~tcmc21/db2/$row[image] alt=\"No image available\" width=25% height=25%> 
 <div class=\"eventdetails\">
-	<p>$row[artist] presents</p>
-	<h2>$row[name]</h2>
+	<p>$row[artist] presents:</p>
+	<div class=\"eventtitle\"><h2>$row[name]</h2></div>
 	<p>@$row[location], $day $date, $row[time]</p>
 	<p>$row[description]</p>
 	<p>
 	<a href=\"$row[ticketlink]\" target=\"_blank\">Get tickets here</a>
 	</p>
+
 </div>	
 </div>
 	";
